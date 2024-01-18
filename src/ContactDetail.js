0fb4f07c9c9e65d182373b813fcf72cd404f577b
najeeb abdalla <92922407+najeebWorld@users.noreply.github.com>
2024-01-18 19:24:31 +0200

    almost there

--- src/ContactDetail.js
+++ src/ContactDetail.js
@@ -1,19 +1,52 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 const ContactDetail = ({ route, navigation }) => {
   const { contact } = route.params;
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Name: {contact.firstName}</Text>
-      <Text>Phone: {contact.phoneNumbers && contact.phoneNumbers[0].number}</Text>
+    <View style={styles.container}>
+      <Text style={styles.contactInfo}>
+        Name: {contact.firstName}{'\n'}
+        Phone: {contact.phoneNumbers && contact.phoneNumbers[0].number}
+      </Text>
 
-      <TouchableOpacity onPress={() => navigation.goBack()}>
-        <Text>Go Back</Text>
+      <TouchableOpacity style={styles.goBackButton} onPress={() => navigation.goBack()}>
+        <Text style={styles.goBackText}>Go Back</Text>
       </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  contactInfo: {
+    textAlign: 'center',
+    marginBottom: 10,
+    borderRadius: 5,
+    borderColor: "black",
+    borderStyle: "solid",
+    borderWidth: 2,
+    backgroundColor: '#f2eaa8', 
+
+    
+  },
+  goBackButton: {
+    backgroundColor: '#f2eaa8', 
+    padding: 10,
+    borderRadius: 5,
+    borderColor: "black",
+    borderStyle: "solid",
+    borderWidth: 2,
+
+  },
+  goBackText: {
+    color: 'black',
+  },
+});
 
 export default ContactDetail;
--- src/ContactsList.js
+++ src/ContactsList.js
@@ -108,6 +108,8 @@ const ContactsList = () => {
           console.log(data[0]);
           setContacts(data);
           setFilteredContacts(data);
+          //console.log(data);//I need to log the data 
+
         }
       }
     })();
